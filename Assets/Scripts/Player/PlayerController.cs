f86225c1becaf0bda0d8421979ddb177ca64355a
canoezbal@gmail.com <canoezbal@gmail.com>
2026-01-29 14:30:18 +0100

    no message

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
@@ -12,17 +13,36 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
 
+    [Header("Coyote Time")]
+    [SerializeField] private float coyoteTime = 0.2f;
+
+    [Header("Input")]
+    [SerializeField] private PlayerInputActions playerInputActions;
+
     private Rigidbody2D _rb;
     private bool _isGrounded;
     private float _moveInput;
+    private float _coyoteTimeCounter;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+
+        // Initialize Input Actions if not assigned
+        if (playerInputActions == null)
+        {
+            playerInputActions = new PlayerInputActions();
+        }
     }
 
     private void OnEnable()
     {
+        // Enable input actions
+        playerInputActions.Enable();
+
+        // Subscribe to jump action
+        playerInputActions.Player.Jump.performed += OnJumpPerformed;
+
         if (GameManager.Instance != null)
         {
             GameManager.Instance.OnPlayerRespawn += OnRespawn;
@@ -31,6 +51,12 @@ public class PlayerController : MonoBehaviour
 
     private void OnDisable()
     {
+        // Unsubscribe from jump action
+        playerInputActions.Player.Jump.performed -= OnJumpPerformed;
+
+        // Disable input actions
+        playerInputActions.Disable();
+
         if (GameManager.Instance != null)
         {
             GameManager.Instance.OnPlayerRespawn -= OnRespawn;
@@ -39,13 +65,17 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Input
-        _moveInput = Input.GetAxisRaw("Horizontal");
+        // Read move input
+        _moveInput = playerInputActions.Player.Move.ReadValue<Vector2>().x;
 
-        // Jump
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        // Coyote Time Counter
+        if (_isGrounded)
         {
-            Jump();
+            _coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            _coyoteTimeCounter -= Time.deltaTime;
         }
     }
 
@@ -58,22 +88,33 @@ public class PlayerController : MonoBehaviour
         _rb.linearVelocity = new Vector2(_moveInput * moveSpeed, _rb.linearVelocity.y);
     }
 
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        // Jump with coyote time
+        if (_coyoteTimeCounter > 0f)
+        {
+            Jump();
+        }
+    }
+
     private void Jump()
     {
         _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+        _coyoteTimeCounter = 0f;
     }
 
     private void OnRespawn(Vector3 position)
     {
         transform.position = position;
         _rb.linearVelocity = Vector2.zero;
+        _coyoteTimeCounter = 0f;
     }
 
     private void OnDrawGizmosSelected()
     {
         if (groundCheck != null)
         {
-            Gizmos.color = Color.red;
+            Gizmos.color = _isGrounded ? Color.green : Color.red;
             Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
     }
